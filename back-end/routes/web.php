b892e4215465ddd987ded11221d82cf6b0f4dc39
Luca Garizio <garizioluca2@gmail.com>
2024-02-29 12:57:14 +0100

    migrations/seeder/factory/model created

--- back-end/routes/web.php
+++ back-end/routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\TagController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -27,5 +27,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/tags', [TagController::class, 'index'])
+    ->name ('tag.index');
 
 require __DIR__.'/auth.php';
