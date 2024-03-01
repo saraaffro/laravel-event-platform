22f6b7067adb39af7b3f55f242d446096027a8a6
saraaffro <saraaffro@gmail.com>
2024-03-01 11:59:05 +0100

    feat: crud hide with auth

--- back-end/resources/views/events/index.blade.php
+++ back-end/resources/views/events/index.blade.php
@@ -2,19 +2,24 @@
 @section('content')
 <div class="container">
     <h1>Events:</h1>
-    <a class="event" href="{{ route('event.create') }}">CREATE NEW EVENT</a>
+    @auth
+        <a class="event" href="{{ route('event.create') }}">CREATE NEW EVENT</a>
+    @endauth
+   
     <ul>
         @foreach ($events as $event)
             <li>
                 <a href="{{ route('event.show', $event -> id)}}">{{ $event -> name }}</a>
-                <a class="edit ms-2" href="{{ route('event.edit', $event -> id) }}">EDIT</a>
-                <form class=" d-inline-block" action="{{route('event.delete', $event)}}" method="POST">
-                    @csrf
-                    @method('DELETE')
 
-                    <input class="delete" type="submit" value="DELETE">
-                </form>
-                
+                @auth
+                    <a class="edit ms-2" href="{{ route('event.edit', $event -> id) }}">EDIT</a>
+                    <form class=" d-inline-block" action="{{route('event.delete', $event)}}" method="POST">
+                        @csrf
+                        @method('DELETE')
+
+                        <input class="delete" type="submit" value="DELETE">
+                    </form> 
+                @endauth
                 <br><br>
             </li>            
         @endforeach
--- back-end/routes/web.php
+++ back-end/routes/web.php
@@ -28,27 +28,33 @@ Route::middleware('auth') -> group(function () {
     Route::get('/profile', [ProfileController::class, 'edit']) -> name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update']) -> name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy']) -> name('profile.destroy');
+
+    // CREATE
+    Route::get('/events/create', [EventController :: class, 'create'])
+    -> name('event.create');
+
+    // STORE
+    Route::post('/events/create', [EventController :: class, 'store'])
+        -> name('event.store');
+
+    // EDIT
+    Route::get('/events/{id}/edit', [EventController :: class, 'edit'])
+        -> name('event.edit');
+
+    // UPDATE
+    Route::put('/events/{id}/update', [EventController :: class, 'update'])
+        -> name('event.update');
+
+    // DELETE
+    Route::delete('/events/{id}', [EventController :: class, 'destroy'])
+        -> name('event.delete');
 });
 
+// INDEX
 Route::get('/events', [EventController :: class, 'index'])
     -> name('event.index');
 
-// ccrud poi vanno nell auth tranne show
-Route::get('/events/create', [EventController :: class, 'create'])
-    -> name('event.create');
-
-Route::post('/events/create', [EventController :: class, 'store'])
-    -> name('event.store');
-
-Route::get('/events/{id}/edit', [EventController :: class, 'edit'])
-    -> name('event.edit');
-
-Route::put('/events/{id}/update', [EventController :: class, 'update'])
-    -> name('event.update');
-
-Route::delete('/events/{id}', [EventController :: class, 'destroy'])
-    -> name('event.delete');
-
+// SHOW
 Route::get('/events/{id}', [EventController :: class, 'show'])
     -> name('event.show');
 
